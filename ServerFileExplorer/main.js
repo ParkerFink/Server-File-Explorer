f94ffd66ddf43438e70f583607ec155705e8c878
Parker Fink <parkeririshgod@gmail.com>
2023-12-10 00:39:10 -0500

    still no work

--- ServerFileExplorer/main.js
+++ ServerFileExplorer/main.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const fs = require('fs')
-
 const app = express()
+const fs = require('fs')
+const path = require('path')
 
-saveDir = './Files'
+
 
 app.use(express.urlencoded({extended: true}))
-app.use(express.static('Files'))
+app.use(express.static('public/Files'))
 app.set('view engine', 'ejs')
+
 
 const port = 6969
 const ip = '127.0.0.1'
@@ -24,15 +25,8 @@ app.get('/', function(req, res){
 
 app.post('/', function(req,res){
 
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.filepath;
-      var newpath = 'C:/Users/Parker Fink/' + files.filetoupload.originalFilename;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-       
-      });
-    })
+  
+  
 })
 
 
