9c2743be287f9c8d7201a8ebe82fcec12af60ac7
Parker Fink <parkeririshgod@gmail.com>
2023-12-12 13:02:26 -0500

    added Download button
    -Added download button
    -Removed download link

--- ServerFileExplorer/main.js
+++ ServerFileExplorer/main.js
@@ -12,11 +12,12 @@ app.use(express.static('./Files'))
 app.use(express.static('./CSS'))
 app.set('view engine', 'ejs')
 
+const upload = require('./upload');
+
 
 
 //reads version file
 const version = fs.readFileSync('version.txt')
-const upload = require('./upload');
 
 //reads config.json
 let parsedFile = JSON.parse(fs.readFileSync('config.json'))
@@ -31,6 +32,7 @@ const setupConfig = {
 //main directory
 app.get('/', function(req, res){
     let File = fs.readdirSync('./Files')
+    console.log(File)
     res.render('index.ejs', {
         Files: File,
         version: version
@@ -50,6 +52,5 @@ app.listen(setupConfig.port, setupConfig.ip, function(){
     console.log("Server IP: " + setupConfig.ip)
     console.log("Server Port: " + setupConfig.port)
     console.log("Server Version:", JSON.parse(version))
-
     
 })
