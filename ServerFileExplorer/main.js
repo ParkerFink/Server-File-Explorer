0db6b54213a31a87e0bfcb7afed2de9bcc7e6cbb
Parker Fink <parkeririshgod@gmail.com>
2023-12-17 21:15:33 -0500

    removed all jzip code
    -temporarily removed all jzip code for it to be redone
    -updated version.txt to the most current version.

--- ServerFileExplorer/main.js
+++ ServerFileExplorer/main.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-const JSZip = require('jszip')
+
 
 
 
@@ -13,7 +13,7 @@ app.use(express.static('./CSS'))
 app.set('view engine', 'ejs')
 
 const upload = require('./upload');
-const JSZip = require('jszip')
+
 
 
 
