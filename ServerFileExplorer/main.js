b07b0d3284065a478e7ca96f8ce87d38d0b097b2
Parker Fink <parkeririshgod@gmail.com>
2024-07-03 11:17:46 -0400

    displays file in Storage folder
    -added storage folder and displays items to screen
    -comments in main.js

--- ServerFileExplorer/main.js
+++ ServerFileExplorer/main.js
@@ -1,15 +1,25 @@
+//node modules
 const express = require("express")
 const app = express()
+const fs = require('fs')
 
+//setup
 app.use(express.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
+app.use(express.static('Storage'))
+const storageFolder = 'Storage/'
 
-
+//endpoints
 app.get('/', function(req,res){
-    res.render('index.ejs')
+    let filenames = fs.readdirSync(storageFolder)
+    console.log(filenames)
+    res.render('index.ejs', {
+        files: filenames
+    })
+
 })
 
-
+//listen server
 app.listen(8080, '127.0.0.1', function(){
     console.log("Server Running")
 })
